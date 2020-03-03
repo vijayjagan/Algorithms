070d02d3cf5b2033989a27f3fa4e1d2b0cb4c553
VJ <vijayj1997@gmail.com>
2020-03-03 11:40:10 -0800

    Add files via upload

--- Day-7.cpp
+++ Day-7.cpp
@@ -1,46 +1,42 @@
-#include <iostream>
-using namespace std;
+/*
+Given a sorted array nums, remove the duplicates in-place such that each element appear only once and return the new length.
 
-bool condition(int a, int b)
-{
-    if ((a % 2 == 0 && b % 2 == 0 )|| (a % 2 != 0 && b % 2 != 0)) 
-        return true;
-    return false;
-}
-void pattern(int n)
-{
-    int m = n + (n - 1);
-    int left = 0;
-    int right = m - 1;
-    for (int i = 0; i < m; i++)
-    {
-        for (int j = 0; j < m; j++)
-        {
-            if (left == j || right == j || (left > j && condition(j, left)) || (right < j && condition(j, right)))
-            {
-                cout << "*";
-            }
-            else
-            {
-                cout << " ";
-            }
+Do not allocate extra space for another array, you must do this by modifying the input array in-place with O(1) extra memory.
+
+Example 1:
+
+Given nums = [1,1,2],
+
+Your function should return length = 2, with the first two elements of nums being 1 and 2 respectively.
+
+It doesn't matter what you leave beyond the returned length.
+*/
+
+#include <iostream>
+
+using namespace std;
+int removeDuplicates(int nums[] , int size) {
+    
+    int value = nums[0]; int count =0;
+    for(int i=1;i<size;i++ ){
+        if(value!=nums[i]){
+            count++;
+            value = nums[i];
+            nums[count]=nums[i];
         }
-        if (i>=m/2)
-        {
-            left--;
-            right++;
-        }
-        else
-        {
-         left++;
-         right--;
-        }
-       
-       cout<<endl;
     }
+    for(int i = 0;i<count;i++){
+        cout<<nums[i];
+    }
+    cout<<endl;
+    return count;
 }
+
 int main()
 {
-    int n = 10;
-    pattern(n);
+
+    int nums[] = {0,0,1,1,1,2,2,3,3,4,5,5,6,7,8,9,10};
+    cout<<removeDuplicates(nums,sizeof(nums)/sizeof(int))<<endl;
+    return 0;
 }
+
